1ca9d17899e61dff09d5a62f2c115957e5b421d9
matrixbirds <yooobuntu@gmail.com>
2020-10-30 21:19:07 +0800

    bump new version

---
+++ src/utils/platform.ts
@@ -0,0 +1,7 @@
+import { BizLogger } from "./biz-logger";
+
+BizLogger.info(`CURRENT RUNTIME: ${process.env.REACT_APP_RUNTIME_PLATFORM}`);
+
+export const isElectron = process.env.REACT_APP_RUNTIME_PLATFORM === 'electron'
+
+export const platform = process.env.REACT_APP_RUNTIME_PLATFORM as string;
